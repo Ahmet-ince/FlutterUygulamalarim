1a21395fae918ba82e87acdb0a67b44fe431a812
Ahmet Zeki Ince <ahmetzekiince@outlook.com>
2023-10-31 14:13:11 +0300

    stream.dart has been changed.

--- stream_sample1.1/lib/stream.dart
+++ stream_sample1.1/lib/stream.dart
@@ -9,14 +9,22 @@ main() {
   void functionForStreamController() async {
     for (int i = 0; i < 10; i++) {
       await Future.delayed(const Duration(seconds: 1));
+      if (i == 6) {
+        _myStreamController.addError('Bir hata oluştu.');
+      }
       _myStreamController.sink.add(i + 1);
     }
+    _myStreamController.close();
   }
 
   functionForStreamController();
   _myStreamController.stream.listen((event) {
     print(event * 10);
-  });
+  }, onDone: () {
+    print('Stream Listen içerisinde onDone fonksiyonu çalıştı.');
+  }, onError: (Error) {
+    print(Error);
+  }, cancelOnError: true);
 }
 
 // Stream<int> myStreamFunction() async* {
